619f1728d8d24407c0fbb0fe0024f5acaba5bc2c
Tristan Rechenberger <rechenberger@me.com>
2025-03-21 15:25:15 +0100

    ➕ better auth

--- src/auth/LoginForm.tsx
+++ src/auth/LoginForm.tsx
@@ -1,78 +1,41 @@
-import {
-  streamDialog,
-  superAction,
-} from '@/super-action/action/createSuperAction'
-import { CredentialsSignin } from 'next-auth'
-import { EmailNotVerifiedAuthorizeError } from './CredentialsProvider'
+'use client'
+
 import { LoginFormClient } from './LoginFormClient'
 import { LoginSocial } from './LoginSocial'
-import { signIn } from './betterAuthClient'
-import { registerUser } from './registerUser'
+import { authClient, signIn } from './betterAuthClient'
 
 export const LoginForm = ({ redirectUrl }: { redirectUrl?: string }) => {
   return (
     <>
       <LoginFormClient
         action={async (data) => {
-          'use server'
-          return superAction(async () => {
-            if (data.type === 'login') {
-              // LOGIN
-              try {
-                await signIn.email({
-                  email: data.email,
-                  password: data.password,
-                })
-              } catch (error) {
-                if (error instanceof CredentialsSignin) {
-                  throw new Error('Invalid credentials')
-                } else if (error instanceof EmailNotVerifiedAuthorizeError) {
-                  // throw new Error('Email not verified')
-                  streamDialog({
-                    title: 'Email not verified',
-                    content: (
-                      <>
-                        <p>
-                          We sent you another verification email to
-                          {data.email}.
-                        </p>
-                        <p>
-                          Please open the email and click sign in to verify your
-                          email.
-                        </p>
-                      </>
-                    ),
-                  })
-                  // await signIn('nodemailer', {
-                  //   email: data.email,
-                  //   redirect: false,
-                  // })
-                  throw new Error('TODO: send verification email')
-                } else {
-                  throw error
-                }
-              }
-              return
-            } else if (data.type === 'register') {
-              // REGISTER
-              await registerUser(data)
-              // await signIn('nodemailer', data)
-              throw new Error('TODO: signup')
-            } else if (data.type === 'forgotPassword') {
-              // CHANGE PASSWORD
-              let redirectTo = '/auth/change-password'
-              if (redirectUrl) {
-                redirectTo += `?redirect=${encodeURIComponent(redirectUrl)}`
-              }
-              // await signIn('nodemailer', {
-              //   email: data.email,
-              //   redirectTo,
-              // })
-              throw new Error('TODO: forgot password')
-            } else {
-              const exhaustiveCheck: never = data
+          if (data.type === 'login') {
+            // LOGIN
+            await signIn.email({
+              email: data.email,
+              password: data.password,
+            })
+          } else if (data.type === 'register') {
+            // REGISTER
+            await authClient.signUp.email({
+              email: data.email,
+              password: data.password,
+              name: data.email,
+            })
+          } else if (data.type === 'forgotPassword') {
+            // CHANGE PASSWORD
+            let redirectTo = '/auth/change-password'
+            if (redirectUrl) {
+              redirectTo += `?redirect=${encodeURIComponent(redirectUrl)}`
             }
-          })
+            // await signIn('nodemailer', {
+            //   email: data.email,
+            //   redirectTo,
+            // })
+            throw new Error('TODO: forgot password')
+          } else {
+            const exhaustiveCheck: never = data
+          }
         }}
         alternatives={
           <>

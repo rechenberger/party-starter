f34328c938a6ff43f0dcd34ec25e289ef740616a
Tristan Rechenberger <rechenberger@me.com>
2025-03-21 17:11:54 +0100

    ➕ better auth

--- src/auth/LoginForm.tsx
+++ src/auth/LoginForm.tsx
@@ -1,43 +1,56 @@
-'use client'
-
+import { superAction } from '@/super-action/action/createSuperAction'
+import { redirect } from 'next/navigation'
 import { LoginFormClient } from './LoginFormClient'
 import { LoginSocial } from './LoginSocial'
-import { authClient, signIn } from './betterAuthClient'
+import { auth } from './auth'
 
 export const LoginForm = ({ redirectUrl }: { redirectUrl?: string }) => {
   return (
     <>
       <LoginFormClient
         action={async (data) => {
-          if (data.type === 'login') {
-            // LOGIN
-            await signIn.email({
-              email: data.email,
-              password: data.password,
-              callbackURL: redirectUrl,
-            })
-          } else if (data.type === 'register') {
-            // REGISTER
-            await authClient.signUp.email({
-              email: data.email,
-              password: data.password,
-              name: data.email,
-              callbackURL: redirectUrl,
-            })
-          } else if (data.type === 'forgotPassword') {
-            // FORGOT PASSWORD
-            let redirectTo = '/auth/change-password'
-            if (redirectUrl) {
-              redirectTo += `?redirect=${encodeURIComponent(redirectUrl)}`
+          'use server'
+          return superAction(async () => {
+            if (data.type === 'login') {
+              // LOGIN
+              await auth.api.signInEmail({
+                body: {
+                  email: data.email,
+                  password: data.password,
+                  callbackURL: redirectUrl,
+                },
+              })
+              if (redirectUrl) {
+                redirect(redirectUrl)
+              }
+            } else if (data.type === 'register') {
+              // REGISTER
+              await auth.api.signUpEmail({
+                body: {
+                  email: data.email,
+                  password: data.password,
+                  name: data.email,
+                  callbackURL: redirectUrl,
+                },
+              })
+              if (redirectUrl) {
+                redirect(redirectUrl)
+              }
+            } else if (data.type === 'forgotPassword') {
+              // FORGOT PASSWORD
+              let redirectTo = '/auth/change-password'
+              if (redirectUrl) {
+                redirectTo += `?redirect=${encodeURIComponent(redirectUrl)}`
+              }
+              // await signIn('nodemailer', {
+              //   email: data.email,
+              //   redirectTo,
+              // })
+              throw new Error('TODO: forgot password')
+            } else {
+              const exhaustiveCheck: never = data
             }
-            // await signIn('nodemailer', {
-            //   email: data.email,
-            //   redirectTo,
-            // })
-            throw new Error('TODO: forgot password')
-          } else {
-            const exhaustiveCheck: never = data
-          }
+          })
         }}
         alternatives={
           <>

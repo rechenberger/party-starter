24cef636c813e92d75857900b76e88a5c75d5aff
Tristan Rechenberger <rechenberger@me.com>
2025-03-24 11:49:12 +0100

    ➕ better-auth

--- src/auth/changePassword.ts
+++ src/auth/changePassword.ts
@@ -1,3 +1,6 @@
+import { db } from '@/db/db'
+import { schema } from '@/db/schema-export'
+import { and, eq } from 'drizzle-orm'
 import { headers } from 'next/headers'
 import { auth } from './auth'
 
@@ -8,11 +11,32 @@ export const changePassword = async ({
   userId: string
   password: string
 }) => {
-  // TODO: does not work when user already has a password
-  await auth.api.setPassword({
-    body: {
-      newPassword: password,
-    },
-    headers: await headers(),
+  const existingPassword = await db.query.account.findFirst({
+    where: and(
+      eq(schema.account.userId, userId),
+      eq(schema.account.providerId, 'credential'),
+    ),
   })
+
+  // Delete the existing password if it exists
+  if (existingPassword) {
+    await db
+      .delete(schema.account)
+      .where(eq(schema.account.id, existingPassword.id))
+  }
+
+  try {
+    await auth.api.setPassword({
+      body: {
+        newPassword: password,
+      },
+      headers: await headers(),
+    })
+  } catch (error) {
+    if (existingPassword) {
+      // restore the existing password on fail
+      await db.insert(schema.account).values(existingPassword)
+    }
+    throw error
+  }
 }

591f5b39354fc1fe5592ca600f090f55714b412f
Sean Nicholas Dieterle <dieterle@sodefa.de>
2025-04-23 16:00:21 +0200

    💄 main top header

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,20 +1,14 @@
 import { AppSidebar } from '@/components/app-sidebar'
 import { TopHeader } from '@/components/TopHeader'
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from '@/components/ui/sidebar'
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <SidebarProvider>
         <AppSidebar />
-        <SidebarInset>
-          <TopHeader disableOffset disableSeparator>
-            <SidebarTrigger className="-ml-1" />
-          </TopHeader>
+        <SidebarInset className="group/topheader">
+          <TopHeader disableSeparator hideIfSecondTopHeaderExists></TopHeader>
           {children}
         </SidebarInset>
       </SidebarProvider>
--- src/components/TopHeader.tsx
+++ src/components/TopHeader.tsx
@@ -1,24 +1,29 @@
 import { cn } from '@/lib/utils'
 import { Separator } from './ui/separator'
+import { SidebarTrigger } from './ui/sidebar'
 
 export const TopHeader = ({
   children,
-  disableOffset: disableOffset = false,
   disableSeparator = false,
+  hideIfSecondTopHeaderExists = false,
 }: {
-  children: React.ReactNode
+  children?: React.ReactNode
   disableOffset?: boolean
   disableSeparator?: boolean
+  hideIfSecondTopHeaderExists?: boolean
 }) => {
   return (
     <div
+      data-topheader
       className={cn(
+        'topheader',
         'flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12',
-        !disableOffset &&
-          '-mt-16 ml-8 group-has-data-[collapsible=icon]/sidebar-wrapper:-mt-12',
+        hideIfSecondTopHeaderExists &&
+          'group-has-[.topheader:nth-of-type(2)]/topheader:hidden',
       )}
     >
-      <div className="flex items-center gap-4 px-4">
+      <div className="flex items-center gap-2 px-4">
+        <SidebarTrigger className="-ml-1" />
         {!disableSeparator && (
           <Separator
             orientation="vertical"

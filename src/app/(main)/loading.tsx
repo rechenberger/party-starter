3066aea821ff6e01f0f8d3688d94617c5db6559c
Sean Nicholas Dieterle <dieterle@sodefa.de>
2025-04-24 17:53:17 +0200

    💄

---
+++ src/app/(main)/loading.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Page() {
+  // TODO: Make this a component
+  return (
+    <div className="bg-background flex-1 w-full flex flex-col items-center justify-center">
+      <div className="mx-8 animate-pulse">
+        <Image src={'/logo.svg'} width={200} height={200} alt="logo" priority />
+      </div>
+    </div>
+  )
+}

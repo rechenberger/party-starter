514061c651d7a4044cf756368000821e155a7f35
Sean Nicholas Dieterle <dieterle@sodefa.de>
2025-04-24 10:20:35 +0200

    💄 header

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -8,8 +8,12 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuItem,
   SidebarRail,
 } from '@/components/ui/sidebar'
+import { PartyPopperIcon } from 'lucide-react'
+import Link from 'next/link'
 import { Suspense } from 'react'
 import { NavMain } from './nav-main'
 import { TeamSwitcher } from './team-switcher'
@@ -22,6 +26,26 @@ export async function AppSidebar({
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <div className="hidden p-2 group-data-[collapsible=icon]:block">
+              <Link href="/">
+                <PartyPopperIcon className="size-4" />
+              </Link>
+            </div>
+            <Link
+              href="/"
+              className="flex flex-row items-center gap-2 w-full overflow-hidden p-2 group-data-[collapsible=icon]:hidden"
+            >
+              <div className="text-xl">
+                <strong>
+                  Party <span className="text-primary">Starter</span>
+                </strong>
+              </div>
+            </Link>
+          </SidebarMenuItem>
+        </SidebarMenu>
+
         <Suspense fallback={<Skeleton className="w-full h-[48px]" />}>
           <TeamSwitcher />
         </Suspense>

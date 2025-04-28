5a70a9e613be892bb5fe3850aec72c0e730443ab
Sean Nicholas Dieterle <dieterle@sodefa.de>
2025-04-28 10:08:36 +0200

    ♻️

--- src/lib/starter.config.tsx
+++ src/lib/starter.config.tsx
@@ -1,3 +1,3 @@
-export const orgs = {
+export const ORGS = {
   onlyAdminsCanCreateOrgs: false,
 }
--- src/organization/canUserCreateOrg.ts
+++ src/organization/canUserCreateOrg.ts
@@ -1,7 +1,7 @@
 import { getMyUser } from '@/auth/getMyUser'
-import { orgs } from '@/lib/starter.config'
+import { ORGS } from '@/lib/starter.config'
 
 export const canUserCreateOrg = async () => {
   const user = await getMyUser()
-  return orgs.onlyAdminsCanCreateOrgs ? user?.isAdmin : !!user
+  return ORGS.onlyAdminsCanCreateOrgs ? user?.isAdmin : !!user
 }

237e2ff859d8c789b77b58bbf9d60df7d19d461b
Sean Nicholas Dieterle <dieterle@sodefa.de>
2025-05-07 14:05:22 +0200

    ✨ ssg landing pages

--- src/app/(landing)/[locale]/layout.tsx
+++ src/app/(landing)/[locale]/layout.tsx
@@ -1,5 +1,12 @@
 import { MainTopLayout } from '@/components/layout/MainTopLayout'
+import { LOCALES } from '@/i18n/locale'
 import { ParamsWrapper } from '@/lib/paramsServerContext'
+
+export const generateStaticParams = async () => {
+  return LOCALES.map((locale) => ({
+    locale,
+  }))
+}
 
 export default ParamsWrapper(
   async ({ children }: { children: React.ReactNode }) => {

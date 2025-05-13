0bca57779021f599176146e1434e294554091a1b
Tim Weiskopf <weiskopf@sodefa.de>
2025-05-13 17:30:00 +0200

    ⚰️

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,8 +1,5 @@
 import { StartPage } from '@/components/StartPage'
-import { getMyLocale } from '@/i18n/getMyLocale'
 import { ParamsWrapper } from '@/lib/paramsServerContext'
-import { LOCALIZATION } from '@/lib/starter.config'
-import { redirect } from 'next/navigation'
 
 export default ParamsWrapper(async () => {
   return <StartPage />

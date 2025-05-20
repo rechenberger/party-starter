02ea3f839a2a198bbf7f390d1f418a914ac37df7
Tristan Rechenberger <rechenberger@me.com>
2025-05-20 12:08:58 +0200

    ✨ / redirects to app if logged in

--- src/app/route.ts
+++ src/app/route.ts
@@ -1,7 +1,18 @@
+import { getIsLoggedIn } from '@/auth/getMyUser'
 import { getMyLocale } from '@/i18n/getMyLocale'
 import { redirect } from 'next/navigation'
 
 export const GET = async () => {
+  const isLoggedIn = await getIsLoggedIn()
+
+  // TODO: redirect to latest org?
+
+  // Redirect to dashboard
+  if (isLoggedIn) {
+    redirect('/app')
+  }
+
+  // Redirect to landing page
   const locale = await getMyLocale()
   redirect(`/${locale}`)
 }

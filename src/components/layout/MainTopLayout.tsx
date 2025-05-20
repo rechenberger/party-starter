43a09b7d6165387edfea018be58bc6eba11d6eb5
Tim Weiskopf <weiskopf@sodefa.de>
2025-05-20 13:38:49 +0200

    ⚰️

--- src/components/layout/MainTopLayout.tsx
+++ src/components/layout/MainTopLayout.tsx
@@ -4,7 +4,6 @@ export const MainTopLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <MainTop />
-      {/* <hr /> */}
       <div className="container flex flex-col gap-8 py-8 flex-1">
         {children}
       </div>

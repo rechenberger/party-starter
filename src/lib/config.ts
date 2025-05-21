3a3b1dd832587b8340efa4a0262170f793200aa9
Tristan Rechenberger <rechenberger@me.com>
2025-05-21 12:44:12 +0200

    💚  BASE_URL

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,1 +1,5 @@
-export const BASE_URL = process.env.BASE_URL || process.env.VERCEL_URL || 'http://localhost:3000'
+export const BASE_URL = process.env.BASE_URL
+  ? process.env.BASE_URL
+  : process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : 'http://localhost:3000'

9cf165a00bb793c51860de0e95737966b4bf1ac4
Tim Weiskopf <weiskopf@sodefa.de>
2025-05-22 10:56:09 +0200

    ♻️

--- src/app/(landing)/[locale]/page.tsx
+++ src/app/(landing)/[locale]/page.tsx
@@ -1,9 +1,8 @@
 import { StartPage } from '@/components/StartPage'
 import { generateMetadataLocalized } from '@/i18n/generateMetadataLocalized'
-import { ParamsWrapper } from '@/lib/paramsServerContext'
 
 export const generateMetadata = generateMetadataLocalized()
 
-export default ParamsWrapper(async () => {
+export default async function Page() {
   return <StartPage />
-})
+}

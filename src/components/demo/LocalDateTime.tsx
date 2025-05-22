f182f9045c47480d088bb84e6b12f1d2abf4328b
Sean Nicholas Dieterle <dieterle@sodefa.de>
2025-05-22 13:14:53 +0200

    ✨ locale in LocalDateTime

--- src/components/demo/LocalDateTime.tsx
+++ src/components/demo/LocalDateTime.tsx
@@ -1,6 +1,9 @@
 'use client'
 
+import { useLocale } from '@/i18n/useLocale'
+
 export const LocalDateTime = ({ datetime }: { datetime: string }) => {
+  const locale = useLocale()
   const date = new Date(datetime)
-  return <>{date.toLocaleString('de')}</>
+  return <>{date.toLocaleString(locale)}</>
 }

083a5946c45399d6d54ca42dc68e73a77df89c17
Tim Weiskopf <weiskopf@sodefa.de>
2025-06-02 16:26:10 +0200

    ♻️

--- src/organization/getCurrentOrgSlug.ts
+++ src/organization/getCurrentOrgSlug.ts
@@ -3,11 +3,7 @@ import { z } from 'zod'
 
 export const getCurrentOrgSlug = async () => {
   const params: unknown = await unstable_rootParams()
-  if (!params) {
-    console.error('No params found in context')
-    console.error('Did you forget to wrap your page in ParamsWrapper?')
-    return null
-  }
+
   const { orgSlug } = z
     .object({
       orgSlug: z.string().nullish(),

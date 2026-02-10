2b47269c6772866149c9cf7e40c21c7c6cd52eea
Tristan Rechenberger <rechenberger@me.com>
2026-02-10 11:39:56 +0100

    Remove redundant refresh calls

--- src/lib/launchpads.ts
+++
@@ -1,7 +0,0 @@
-import { createTeampilotClient } from '@teampilot/sdk'
-
-export const launchpads = createTeampilotClient({
-  default: {
-    launchpadSlugId: process.env.TEAMPILOT_DEFAULT_LAUNCHPAD_SLUG_ID!,
-  },
-})

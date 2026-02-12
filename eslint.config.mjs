eda806d8d9c2a7259bc105199d6a5ded36d0c1e7
Tristan Rechenberger <rechenberger@me.com>
2026-02-12 09:21:20 +0100

    Exclude .e2e-artifacts from lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,10 @@
 import nextCoreWebVitals from 'eslint-config-next/core-web-vitals'
 
-const eslintConfig = [...nextCoreWebVitals]
+const eslintConfig = [
+  ...nextCoreWebVitals,
+  {
+    ignores: ['.e2e-artifacts/**'],
+  },
+]
 
 export default eslintConfig
